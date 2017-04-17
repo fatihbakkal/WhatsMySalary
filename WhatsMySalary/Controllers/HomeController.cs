bffc9c5dbcb2c070b56012589ab2d9e9e9f6e1fc
Fatih BAKKAL <Fatih BAKKAL@FATIH-VAIO>
2017-04-17 22:19:42 +0300

    Fix

--- WhatsMySalary/Controllers/HomeController.cs
+++ WhatsMySalary/Controllers/HomeController.cs
@@ -13,5 +13,49 @@ namespace WhatsMySalary.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public int GetElapsedTime(int salaryDay)
+        {
+            int elapsedTime;
+
+            if (salaryDay > DateTime.Today.Day)
+            {
+                var dif = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) - salaryDay;
+
+                elapsedTime = (DateTime.Now.Day * 24 * 60 * 60) + (DateTime.Now.Hour * 60 * 60) + (DateTime.Now.Minute * 60) + (DateTime.Now.Second) 
+                    + (dif * 24 * 60 * 60);
+            }
+            else
+            {
+                elapsedTime = (DateTime.Now.Day * 24 * 60 * 60) + (DateTime.Now.Hour * 60 * 60) + (DateTime.Now.Minute * 60) + (DateTime.Now.Second) 
+                    - (salaryDay * 24 * 60 * 60); ;
+            }
+
+            return elapsedTime;
+        }
+
+        [HttpGet]
+        public int GetRemainTime(int salaryDay)
+        {
+            int remainTime;
+
+            if (salaryDay > DateTime.Today.Day)
+            {
+                var dif = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) - salaryDay;
+
+                remainTime = (DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) * 24 * 60 * 60) 
+                    - ((DateTime.Now.Day * 24 * 60 * 60) + (DateTime.Now.Hour * 60 * 60) + (DateTime.Now.Minute * 60) + (DateTime.Now.Second)) 
+                    - (dif * 24 * 60 * 60);
+            }
+            else
+            {
+                remainTime = (DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) * 24 * 60 * 60) 
+                    - ((DateTime.Now.Day * 24 * 60 * 60) + (DateTime.Now.Hour * 60 * 60) + (DateTime.Now.Minute * 60) + (DateTime.Now.Second)) 
+                    + (salaryDay * 24 * 60 * 60);
+            }
+
+            return remainTime;
+        }
     }
 }
